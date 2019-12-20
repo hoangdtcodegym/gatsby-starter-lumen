682b356412366bea0cb259c52a0e4662063d9930
Dư Thanh Hoàng <duthanhhoang@MacOnes-MacBook-Pro.local>
2019-12-20 15:31:40 +0700

    fix post

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -50,7 +50,7 @@ export const query = graphql`
     allMarkdownRemark(
         limit: $postsLimit,
         skip: $postsOffset,
-        filter: { frontmatter: { template: { eq: "post" } },
+        filter: { frontmatter: { template: { eq: "post" } } },
         sort: { order: DESC, fields: [frontmatter___date] }
       ){
       edges {
